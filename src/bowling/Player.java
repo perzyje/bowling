623d6155c6540ac11da74adc5f5d836db7a08d85
georgekoehl <gk401@lab.icc.edu>
2015-01-15 06:00:32 -0600

    Update Player.java

--- src/bowling/Player.java
+++ src/bowling/Player.java
@@ -4,13 +4,47 @@
  * and open the template in the editor.
  */
 package bowling;
-
+import java.util.ArrayList; 
 /**
  *
  * @author 
  */
 public class Player {
+    //===============Variables  
+    private final String NAME; 
+    private final int TOTAL_PINS = 10; //number of pin in a frame
+    private ArrayList<Frame> game = new ArrayList<Frame>(); //Array of frame objects
+    private int frameCount = 0; 
+    private int strikeCount = 0; 
+    private int spareCount = 0; 
+    private int totalScore = 0; 
+    
+    
+//================Constructor 
+    
+    public Player (String name)
+    {
+        this.NAME = name; 
+    }//eoconstructor String 
+    
+//================Mutators 
+    
+    public void addFrame(int ballOne, int ballTwo )
+    {
+        //Check that the total number of pins recorded are 
+        //equal to or less than the total number of pins 
+        //per frame
+        if (ballOne + ballTwo <= TOTAL_PINS)
+        {
+            //add a valid frame to the game array
+            Frame currentFrame = new Frame(ballOne, ballTwo); 
+            game.add(currentFrame); 
+            frameCount++; //increment the frame count 
+        }
+    
+    }//eomethod addFrame
     
     
     
-}
+}//EOCLASS Player
+
