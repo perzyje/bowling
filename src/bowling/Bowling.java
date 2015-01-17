e06ee7011de5d4fb73e1fc192074666c50193a5d
georgekoehl73 <georgekoehl73@outlook.com>
2015-01-17 10:07:55 -0600

    Added a Static method to test the Player Object

--- src/bowling/Bowling.java
+++ src/bowling/Bowling.java
@@ -16,6 +16,48 @@ public class Bowling {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        BowlingDemo(); //run the bowling demo 
+        
+        
+    }//eoMain
     
-}
+    
+    /** 
+     * Static Method to work on the self-test and test functionality of the 
+     * BowlingGame project.
+     * This will populate a full game of bowling that can be used to further 
+     * test scoring and other implementations  
+     */
+    private static void BowlingDemo(){
+    
+        Player me = new Player("DemoPlayer"); 
+        
+        
+        System.out.println("Current Round Over? " +  me.shot(1)); 
+        System.out.println("Current Round Over? " +  me.shot(0)); 
+        System.out.println("Current Round Over? " +  me.shot(3)); 
+        System.out.println("Current Round Over? " +  me.shot(7)); 
+         System.out.println("Current Round Over? " +  me.shot(1)); 
+        System.out.println("Current Round Over? " +  me.shot(4)); 
+        System.out.println("Current Round Over? " +  me.shot(0)); 
+        System.out.println("Current Round Over? " +  me.shot(9)); 
+         System.out.println("Current Round Over? " +  me.shot(1)); 
+        System.out.println("Current Round Over? " +  me.shot(6)); 
+        System.out.println("Current Round Over? " +  me.shot(0)); 
+        System.out.println("Current Round Over? " +  me.shot(9)); 
+         System.out.println("Current Round Over? " +  me.shot(2)); 
+        System.out.println("Current Round Over? " +  me.shot(4)); 
+        System.out.println("Current Round Over? " +  me.shot(10)); 
+        System.out.println("Current Round Over? " +  me.shot(0)); 
+         System.out.println("Current Round Over? " +  me.shot(1)); 
+        System.out.println("Current Round Over? " +  me.shot(3)); 
+        System.out.println("Current Round Over? " +  me.shot(0)); 
+        System.out.println("Current Round Over? " +  me.shot(9)); 
+        me.shot(1);
+        me.seeGame();
+    
+    
+    
+    }//eomethod BowlingDemo
+    
+}//EOCLASS Bowling 
